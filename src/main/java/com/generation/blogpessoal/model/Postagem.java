f1d25a92af45a57bfba24e125618d1b8c5150ddf
jovesposito <jovesposito@outlook.com>
2024-07-15 11:59:56 -0300

    Implementação do spring security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -37,6 +37,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("Postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("Postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -76,6 +80,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 }
